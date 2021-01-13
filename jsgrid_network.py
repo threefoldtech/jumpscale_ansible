2281e3bdbd2a85692348a11aeaa4faa7a3872556
Omar Elawady <omarelawady1998@gmail.com>
2021-01-13 16:37:13 +0200

    fix documentation

--- jsgrid_network.py
+++ jsgrid_network.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-# Copyright: (c) 2020, Your Name <YourName@example.org>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
@@ -9,49 +7,75 @@ DOCUMENTATION = r'''
 ---
 module: jsgrid_network
 
-short_description: A module to add and remove a network
+short_description: A module to add and remove a network.
 
 version_added: "1.0.0"
 
-description: This is my longer description explaining my test info module.
+description: A module to add and remove a network.
 
+        state=dict(type='str', required=True),
+        identity_name=dict(type='str', required=False),
+        pool_id=dict(type='int', required=False),
+        node_id=dict(type='str', required=False),
+        node_ip_range=dict(type='str', required=False),
+        ip_range=dict(type='str', required=False)
 options:
     name:
-        description: This is the message to send to the test module.
+        description: The network name to be created.
         required: true
+        type: str
+    state:
+        description: present|removed.
+        required: true
+        type: str
+    identity_name:
+        description: The identity instance name registered on the system for the network to be created with.
+        required: false
+        default: The default identity
+        type: str
+    pool_id:
+        description: The pool id to create the network on.
+        required: when creating a network
+        type: int
+    node_id:
+        description: The node to add to the network.
+        required: false
+        default: Any node in the pool.
+        type: str
+    node_ip_range:
+        description: The ip range to be reserved for the node.
+        required: false
+        default: the first 24 subnet of ip_range.
+        type: str
+    ip_range:
+        description: The network ip range.
+        required: false
+        default: 10.100.0.0/16
         type: str
 
 author:
-    - Your Name (@yourGitHubHandle)
+    - Omar Elawady (@OmarElawady)
 '''
 
 EXAMPLES = r'''
 # Pass in a message
-- name: Test with a message
-  my_namespace.my_collection.netowrk_info:
-    name: hello world
+- name: Create a network
+  jsgrid_network:
+    name: management
+    pool_id: 34
+    ip_range: 10.200.0.0/16
+    node_id: 26ZATmd3K1fjeQKQsi8Dr7bm9iSRa3ePsV8ubMcbZEuY
+    node_ip_range: 10.200.1.0/24
+    state: present
+    identity_name: omar
+- name: Remove the created network
+  jsgrid_network:
+    name: management
+    identity_name: omar
 '''
 
 RETURN = r'''
-# These are examples of possible return values, and in general should use other names for return values.
-original_message:
-    description: The original name param that was passed in.
-    type: str
-    returned: always
-    sample: 'hello world'
-message:
-    description: The output message that the test module generates.
-    type: str
-    returned: always
-    sample: 'goodbye'
-my_useful_info:
-    description: The dictionary containing information about your system.
-    type: dict
-    returned: always
-    sample: {
-        'foo': 'bar',
-        'answer': 42,
-    }
+changed: False
 '''
 
 from ansible.module_utils.basic import AnsibleModule
@@ -121,9 +145,7 @@ def validate_creation_fields(module, result):
     if module.params['pool_id'] is None:
         module.fail_json(msg="missing required argument pool_id for network creation", **result)
 
-# name, access_node, ip_range, ip_version, pool_id, identity_name=None
 def run_module():
-    # define available arguments/parameters a user can pass to the module
     module_args = dict(
         name=dict(type='str', required=True),
         state=dict(type='str', required=True),
@@ -133,19 +155,11 @@ def run_module():
         node_ip_range=dict(type='str', required=False),
         ip_range=dict(type='str', required=False)
     )
-    # seed the result dict in the object
-    # we primarily care about changed and state
-    # changed is if this module effectively modified the target
-    # state will include any data that you want your module to pass back
-    # for consumption, for example, in a subsequent task
+
     result = dict(
         changed=False,
     )
 
-    # the AnsibleModule object will be our abstraction working with Ansible
-    # this includes instantiation, a couple of common attr would be the
-    # args/params passed to the execution, as well as if the module
-    # supports check mode
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=True
@@ -170,8 +184,6 @@ def run_module():
         module.fail_json(msg=traceback.format_exc(), **result)
 
 
-    # in the event of a successful module execution, you will want to
-    # simple AnsibleModule.exit_json(), passing the key/value results
     module.exit_json(**result)
 
 
