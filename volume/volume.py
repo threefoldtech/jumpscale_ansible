8772a5e50dac43ba19bcb4467fad2a0e63ee28f7
m-motawea <magedmotawea@gmail.com>
2021-01-17 09:54:42 +0200

    add volume module

---
+++ volume/volume.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import AnsibleModule
+from jumpscale.loader import j
+
+
+
+DOCUMENTATION = r'''
+---
+module: volume
+
+short_description: volume module for zos
+
+version_added: "1.0.0"
+
+description: module to create volumes for TF Grid.
+
+options:
+    identity_name:
+        description: identity name to be used to deploy the container defaults to j.core.identity.me
+        required: False
+        type: str
+    pool_id:
+        description: capacity pool id to deploy the volume in
+        required: True
+        type: int
+    node_id:
+        description: id of the node to deploy the container on
+        required: True
+        type: str
+    size:
+        description: size of the volume in GB
+        required: True
+        type: int
+    type:
+        description: disk type for the volume
+        required: False
+        type: str
+        choices: ssd, hdd
+        default: ssd
+    wait:
+        description: wait for workload to be successful before exit. defaults to True
+        required: False
+        type: bool
+        default: True
+    
+
+author:
+    - Maged Motawea (@m-motawea)
+    
+'''
+
+
+
+RETURN = r'''
+original_message:
+    description: The original name param that was passed in.
+    type: str
+    returned: always
+message:
+    description: The output message that the test module generates.
+    type: dict
+    returned: always
+    sample: "{'wid': 18116, 'message': ''}"
+'''
+
+
+def run_module():
+    module_args = dict(
+        identity_name=dict(type='str', required=False),
+        pool_id=dict(type='int', required=True),
+        node_id=dict(type='str', required=True),
+        size=dict(type='int', required=True),
+        type=dict(type='str', required=False, default='ssd', choices=['ssd', 'hdd']),
+        # wait for workload flag
+        wait=dict(type='bool', required=False, default=True),
+    )
+
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    zos = j.sals.zos.get(module.params['identity_name'])
+    vol = zos.create(
+        node_id=module.params['node_id'],
+        pool_id=module.params['pool_id'],
+        size=module.params['size'],
+        type=module.params['type'].upper()
+    )
+    wid = zos.workloads.deploy(vol)
+
+    result["changed"] = True
+    result["message"] = {"wid": wid, "message": ""}
+
+    if module.params["wait"]:
+        success, msg = zos.workloads.wait(wid)
+        result["changed"] = success
+        result["message"]["message"] = msg
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
